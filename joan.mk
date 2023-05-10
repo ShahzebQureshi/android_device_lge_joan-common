834a43aaf03528d344418a79fbc52824640eb5e3
ShahzebQureshi <55016172+ShahzebQureshi@users.noreply.github.com>
2023-05-10 05:58:14 +0500

    Update joan.mk

--- joan.mk
+++ joan.mk
@@ -6,7 +6,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay \
-    $(COMMON_PATH)/overlay-lineage
+    $(COMMON_PATH)/overlay-arrow
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(COMMON_PATH)/overlay-lineage/lineage-sdk
